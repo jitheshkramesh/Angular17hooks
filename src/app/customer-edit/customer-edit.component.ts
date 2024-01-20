807e4fe9b4b3977f6ab66d8868e106409b9f7764
jitheshkr <87263767+jitheshkr@users.noreply.github.com>
2024-01-20 19:21:28 +0530

    customer update rective

--- src/app/customer-edit/customer-edit.component.ts
+++ src/app/customer-edit/customer-edit.component.ts
@@ -5,7 +5,7 @@ import { Countries } from '../services/countries';
 import { customerService } from '../services/customer.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { CommonModule } from '@angular/common';
+import { CommonModule, formatDate } from '@angular/common';
 import { ICustomer } from '../interfaces/customer.interface';
 import { first } from 'rxjs/operators';
 
@@ -18,7 +18,7 @@ import { first } from 'rxjs/operators';
 })
 export class CustomerEditComponent implements OnInit, OnDestroy {
 
-  customerForm: FormGroup;
+ // customerForm: FormGroup;
   firstNameFormControl: FormControl;
   lastNameFormControl: FormControl;
   emailFormControl: FormControl;
@@ -43,41 +43,61 @@ export class CustomerEditComponent implements OnInit, OnDestroy {
     private router: Router,
     private toastr: ToastrService,
     private route: ActivatedRoute) {
-    this.customerForm = new FormGroup({
-      country: new FormControl(null),
-      gender: new FormControl('', Validators.required)
-    });
+    // this.customerForm = new FormGroup({
+    //   country: new FormControl(null),
+    //   gender: new FormControl('', Validators.required)
+    // });
     // this.customerForm.controls['country'].setValue(this.default, { onlySelf: true });
   }
+
+  editcustomerForm = new FormGroup({
+    firstNameFormControl: new FormControl(''),
+    lastNameFormControl: new FormControl(''),
+    emailFormControl: new FormControl(''),
+    cityFormControl: new FormControl(''),
+    stateFormControl: new FormControl(''),
+    countryFormControl: new FormControl(''),
+    zipcodeFormControl: new FormControl(''),
+    phoneFormControl: new FormControl(''),
+    birthDateFormControl: new FormControl(''),
+    genderFormControl: new FormControl('') 
+  });
 
   ngOnInit(): void {
 
     const id = this.route.snapshot.paramMap.get('id');
     console.log('edit id is :' + id);
 
-    this.customerForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      city: ['', Validators.required],
-      state: ['', Validators.required],
-      country: ['', Validators.required],
-      zipcode: ['', Validators.required],
-      phone: ['', Validators.required],
-      birthDate: ['', Validators.required],
-      gender: ['', Validators.required]
-    });
+
 
 
     this.subscription = this.service.getCustomerById(Number(id)).pipe(first()).subscribe((res: any) => {
-      // this.customerForm.patchValue(res);
+      console.log(res['firstName']);
+      this.editcustomerForm = new FormGroup({
+        firstNameFormControl: new FormControl(res['firstName']),
+        lastNameFormControl:  new FormControl(res['lastName']),
+        emailFormControl:  new FormControl(res['email']),
+        cityFormControl:  new FormControl(res['city']),
+        stateFormControl:  new FormControl(res['state']),
+        countryFormControl:  new FormControl(res['country']),
+        zipcodeFormControl:  new FormControl(res['zipcode']),
+        phoneFormControl:  new FormControl(res['phone']),
+        birthDateFormControl:  new FormControl(res['birthDate']),
+        //new FormControl(res['birthDate']),
+        genderFormControl:  new FormControl(res['gender']),
+        
+      });
+
+      this.editcustomerForm.get('birthDateFormControl').patchValue(this.formatDate(new Date(res['birthDate'])));
+
+      //this.customerForm.patchValue(res);
       // this.customerForm.controls['firstName'].setValue(res.firstName, { onlySelf: true });
       // this.customerForm = this.fb.group({
       //   firstName: res.firstName
       // })
-      this.firstNameFormControl = res.firstName;
+      //this.customerForm.controls['firstName'].setValue(res.firstName);
       console.log('Customer detail is :');
-      console.log(res);
+      console.log( this.editcustomerForm.value );
     }, err => {
       this.toastr.error('Customer', 'Error by Id');
     });
@@ -86,13 +106,38 @@ export class CustomerEditComponent implements OnInit, OnDestroy {
     //this.customerForm = this.cust;
 
     //this.customerForm.controls['firstName'].setValue(id, { onlySelf: true });
-    this.toastr.info('Id', id);
+   // this.toastr.info('Id', id);
+  } 
+
+  private formatDate(date) {
+    const d = new Date(date);
+    let month = '' + (d.getMonth() + 1);
+    let day = '' + d.getDate();
+    const year = d.getFullYear();
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    return [year, month, day].join('-');
   }
 
   onSubmitForm() {
-    console.log(this.customerForm);
-    if (this.customerForm.valid) {
-      this.subscription = this.service.customerUpdate(this.customerForm.value).subscribe(res => {
+    console.log(this.editcustomerForm);
+
+    var inputData = {
+      id: this.route.snapshot.paramMap.get('id'),
+      firstName: this.editcustomerForm.value.firstNameFormControl,
+      lastName: this.editcustomerForm.value.lastNameFormControl,
+      email: this.editcustomerForm.value.emailFormControl,
+      city: this.editcustomerForm.value.cityFormControl,
+      state: this.editcustomerForm.value.stateFormControl,
+      country: this.editcustomerForm.value.countryFormControl,
+      zipcode:this.editcustomerForm.value.zipcodeFormControl,
+      phone: this.editcustomerForm.value.phoneFormControl,
+      birthDate: this.editcustomerForm.value.birthDateFormControl,
+      gender: this.editcustomerForm.value.genderFormControl,
+    };
+
+    if (this.editcustomerForm.valid) {
+      this.subscription = this.service.customerUpdate(inputData).subscribe(res => {
         console.log('Updated successfully');
         this.toastr.success('Customer', 'Updated successfully');
         this.backtoList();
